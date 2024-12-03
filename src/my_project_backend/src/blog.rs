b1391cc93856cf074c5f07ba27a3d2f11d33bae8
Kubolowe z <kubolowe.priv@gmail.com>
2024-12-03 16:36:02 +0000

    Progress

--- src/my_project_backend/src/blog.rs
+++ src/my_project_backend/src/blog.rs
@@ -1,4 +1,23 @@
-struct Blog {
+use candid::CandidType;
+
+#[derive(Clone, CandidType)]
+pub struct Blog {
     title: String,
-    date: u32 // 0 - 255
+    date: u32, // 0 - 255
+    content: String,
+    tags: Vec<String>
+}
+
+impl Blog { // implementacja
+
+    pub fn new(title: String, date: u32, content: String, tags: Vec<String>) -> Self {
+        Self {
+            title,
+            date,
+            content,
+            tags,
+        }
+    }
+
+
 }
--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,10 +1,10 @@
 use std::cell::RefCell;
-
+use crate::blog::Blog;
 
 mod blog; 
 
 thread_local! {
-    static BLOGS: RefCell<Vec<String>> = RefCell::new(Vec::new());
+    static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
 }
 
 
@@ -14,12 +14,13 @@ thread_local! {
 // COMMENTS
 
 #[ic_cdk::update]
-fn add_blog(new_blogs: String) {
-    BLOGS.with(|blogs| blogs.borrow_mut().push(new_blogs))
+fn add_blog(title: String, date: u32, content: String, tags: Vec<String>) {
+    let blog = Blog::new(title, date, content, tags);
+    BLOGS.with(|blogs| blogs.borrow_mut().push(blog))
 }
 
 #[ic_cdk::query]
-fn get_blogs() -> Vec<String> {
+fn get_blogs() -> Vec<Blog> {
     BLOGS.with(|blogs| blogs.borrow().clone())
 }
 
